01c3bb14741f0c9ea441683cbc770fdfe966c869
Aanand Kainth <akainth015@gmail.com>
2019-04-21 22:40:58 -0700

    Clean up the server's code and the standard dashboard

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const uuidv4 = require('uuid/v4');
 
-const indexRouter = require('./routes/index');
+const {config} = require('./package');
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.locals.organization = process.env.npm_package_config_organization;
+Object.assign(app.locals, config);
+
+// setup session manager
+app.set('sessions', new Map());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,23 +22,57 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.route("/log-out")
+    .all(function (req, res) {
+        res.clearCookie(process.env.npm_package_config_session);
+        res.redirect("/");
+    });
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
+// Check if an incoming request has an active session. If it does, continue as usual. Otherwise, require them to log in
+// before allowing them to continue as usual.
+app.use("/", function (req, res, next) {
+    const sessionId = req.cookies[process.env.npm_package_config_session];
+    const sessions = app.get('sessions');
+
+    if (sessions.has(sessionId)) {
+        req.session = sessions.get(sessionId);
+        next();
+    } else {
+        if (req.body.username && req.body.password) {
+            const account = config.accounts.find(
+                account => account.username === req.body.username && account.password === req.body.password);
+            if (account !== undefined) {
+                const sessionId = uuidv4();
+                res.cookie(process.env.npm_package_config_session, sessionId);
+                sessions.set(sessionId, {
+                    account,
+                    createdAt: new Date(),
+                    id: sessionId
+                });
+                console.log(`${account.username} logged in at ${new Date().toLocaleString()}`);
+                res.redirect(req.body.continue);
+            } else {
+                res.status(401);
+                res.render("log-in", {
+                    continueTo: req.path,
+                    title: "Log in",
+                    wrongCredentials: true
+                });
+            }
+        } else {
+            res.status(401);
+            res.render("log-in", {
+                continueTo: req.path,
+                title: "Log in"
+            });
+        }
+    }
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error', {
-        title: "Error"
+app.get("/", function (req, res) {
+    res.render(`portals/${req.session.account.role}`, {
+        session: req.session,
+        title: `Portal - ${req.session.account.username}`
     });
 });
 
---
+++ public/javascripts/portals/standard.js
@@ -0,0 +1,42 @@
+new Chart(document.querySelector("#balance-history canvas"), {
+    data: {
+        datasets: [{
+            backgroundColor: "#79c2ff",
+            borderColor: "#33a6ff",
+            data: [1000, 200, 1100, 800, 1000],
+            label: "Balance History"
+        }],
+        labels: ["January", "February", "March", "April", "May"]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    },
+    type: "line"
+});
+
+new Chart(document.querySelector("#credit-card canvas"), {
+    data: {
+        datasets: [{
+            backgroundColor: ["#2162c2", "#33a6ff"],
+            data: [300, 4700]
+        }],
+        labels: ["Used", "Remaining"],
+    },
+    type: "pie"
+});
+
+document.querySelector("#transaction-history button").addEventListener("click", e => {
+    e.target.style.display = "none";
+    document.querySelector("#transaction-history h2").style.display = "block";
+    const fakeServerError = new Error();
+    fakeServerError.message = "Failed to open 'accounts.json' for reading";
+    fakeServerError.name = "EACCES";
+    fakeServerError.description = "This service does not have permission to read 'accounts.json'";
+    console.error(fakeServerError);
+});
