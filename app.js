c5429d00c27b09e57217c44a4761a3f889029521
Aanand Kainth <aanand@akainth.me>
2021-04-05 11:06:21 -0700

    Update dependencies

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const uuidv4 = require('uuid/v4');
+const {v4: uuidv4} = require("uuid");
 
 const {config} = require('./package');
 
